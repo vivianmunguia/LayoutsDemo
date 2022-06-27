58f195b3187506262cb0391f24999cea5cfdb7a9
Vivian Munguía <andy_munguia@hotmail.com>
2022-06-26 21:36:36 -0700

    Creation of StackLayout, AbsoluteLayout and GridLayout to know how to use them

---
+++ LayoutsDemo/XamarinPage1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LayoutsDemo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class XamarinPage1 : ContentPage
+    {
+        public XamarinPage1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LayoutsDemo/XamarinPage3.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LayoutsDemo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class XamarinPage3 : ContentPage
+    {
+        public XamarinPage3()
+        {
+            InitializeComponent();
+        }
+    }
+}
